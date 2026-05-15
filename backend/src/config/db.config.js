f6bf5524cbd02e9142bffffdb68475f385aed466
ignacio <progremb@gmail.com>
2026-05-15 00:07:14 -0500

    cambiando datos de conexion de la base de datos al de nube

--- backend/src/config/db.config.js
+++ backend/src/config/db.config.js
@@ -7,16 +7,10 @@ console.log("Variables de entorno cargadas, db.config.js ")
 const BD_URL = process.env.DATABASE_URL;
 
 const pool = new pg.Pool({
-  user: process.env.DB_USER,
-  host: process.env.DB_HOST,
-  database: process.env.DB_NAME,
-  password: process.env.DB_PASSWORD,
-  port: process.env.DB_PORT,
-
- /** connectionString: BD_URL,
+  connectionString: BD_URL,
   ssl: {
     rejectUnauthorized: false
-  }*/
+  }
 });
 
 async () => {
