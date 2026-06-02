b7963ccb42cd71de5a66b47aa0794aee0ba8b281
ignacio <progremb@gmail.com>
2026-06-02 12:30:41 -0500

    feature/ prueba de github actions 6

--- backend/src/config/db.config.js
+++ backend/src/config/db.config.js
@@ -8,8 +8,11 @@ const BD_URL = process.env.DATABASE_URL;
 
 let pool
 
-if (!BD_URL || BD_URL.lenght === 0 ) 
-  { 
+console.log("DATABASE_URL existe:", !!process.env.DATABASE_URL);
+console.log("DATABASE_URL longitud:", process.env.DATABASE_URL?.length);
+
+if (!BD_URL || BD_URL.length === 0 ) 
+  {     
     console.log("no existe conexion con la base de datos DESPLEGADA")
 
     pool = new pg.Pool({
